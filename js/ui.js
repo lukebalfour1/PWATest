7dc8136379f750fcf84207c24ee61b48a2e4087a
lukebalfour1 <60646462+lukebalfour1@users.noreply.github.com>
2020-02-13 15:37:12 +0000

    Layout

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,6 @@
 //navbar slide code
 document.addEventListener('DOMContentLoaded', function() {
-    // nav menu
-    const menus = document.querySelectorAll('.side-menu');
-    M.Sidenav.init(menus, {edge: 'left'});
-  });
+  // nav menu
+  const menus = document.querySelectorAll('.side-menu');
+  M.Sidenav.init(menus, {edge: 'left'});
+});
